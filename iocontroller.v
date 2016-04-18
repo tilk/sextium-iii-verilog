00d1ef904b6cd92b0fe5acd2427482bb9416a8b4
Marek Materzok <tilk@tilk.eu>
2016-04-18 21:07:25 +0200

    Towards IO.

---
+++ iocontroller.v
@@ -0,0 +1,61 @@
+`define ST_DECODE 0
+`define ST_HALT 1
+`define ST_WAITACK 2
+`define ST_WAITREADY 3
+`define SYSCALL_HALT 0
+`define SYSCALL_LOAD 1
+`define SYSCALL_STORE 2
+module iocontroller
+(
+	input clock,
+	input reset,
+	input runio,
+	input [15:0] acc,
+	input ioack,
+	output reg iobusy,
+	output reg io_read,
+	output reg io_write,
+	output reg acc_write
+);
+
+	reg [1:0] state;
+
+	always @(negedge clock)
+	begin
+		if(~reset) begin
+			iobusy <= 1;
+			state <= `ST_DECODE;
+		end else begin
+			case(state)
+				`ST_DECODE: begin
+					case(acc)
+						`SYSCALL_HALT: state <= `ST_HALT;
+						`SYSCALL_LOAD: begin
+							io_read <= 1;
+							acc_write <= 1;
+							state <= `ST_WAITACK;
+						end
+						`SYSCALL_STORE: begin
+							io_write <= 1;
+							state <= `ST_WAITACK;
+						end
+					endcase
+				end
+				`ST_HALT: state <= `ST_HALT;
+				`ST_WAITACK: begin
+					if (ioack) begin
+						io_read <= 0;
+						io_write <= 0;
+						iobusy <= 0;
+						state <= `ST_WAITREADY;
+					end
+				end
+				`ST_WAITREADY: begin
+					iobusy <= 1;
+					if (~ioack) state <= `ST_DECODE;
+				end
+			endcase
+		end
+	end
+
+endmodule
--- sextium_core.v
+++ sextium_core.v
@@ -2,6 +2,7 @@ module sextium_core
 (
    input clock,
    input reset,
+	input ioack,
    inout [15:0] io_bus,
    inout [15:0] mem_bus,
    output [15:0] addr_bus,
@@ -11,10 +12,10 @@ module sextium_core
 	output io_write
 );
 
-	wire acc_write, ar_write, dr_write, ir_write, ip_write;
+	wire acc_write, io_acc_write, ar_write, dr_write, ir_write, ip_write;
 	wire [15:0] acc_in, acc_out, ar_in, ar_out, dr_in, dr_out, ir_in, ir_out, ip_in, ip_out, alu_out, swap_out;
 	wire [15:0] ip_next, ip_src;
-	wire accz, accn;
+	wire accz, accn, runio, iobusy;
 
 	wire [3:0] insn;
 	wire seladdr, selswap, doswap, selip1, selip2;
@@ -34,17 +35,19 @@ module sextium_core
 	assign accz = acc_out == 16'h0;
 	assign accn = acc_out[15];
 	
-	register acc_register(.clock(clock), .reset(reset), .write(acc_write), .indata(acc_in), .outdata(acc_out));
+	register acc_register(.clock(clock), .reset(reset), .write(io_acc_write | acc_write), .indata(acc_in), .outdata(acc_out));
 	register ar_register(.clock(clock), .reset(reset), .write(ar_write), .indata(ar_in), .outdata(ar_out));
 	register dr_register(.clock(clock), .reset(reset), .write(dr_write), .indata(dr_in), .outdata(dr_out));
 	register ir_register(.clock(clock), .reset(reset), .write(ir_write), .indata(ir_in), .outdata(ir_out));
 	register ip_register(.clock(clock), .reset(reset), .write(ip_write), .indata(ip_in), .outdata(ip_out));
 	
-	controller sextium_controller(.clock(clock), .reset(reset), .insn(insn), .accz(accz), .accn(accn), 
-		.mem_read(mem_read), .mem_write(mem_write), .io_read(io_read), 
-		.io_write(io_write), .ir_write(ir_write), .ip_write(ip_write), .acc_write(acc_write),
+	controller sextium_controller(.clock(clock), .reset(reset), .insn(insn), .accz(accz), .accn(accn), .iobusy(iobusy),
+		.mem_read(mem_read), .mem_write(mem_write), .ir_write(ir_write), .ip_write(ip_write), .acc_write(acc_write),
 		.seladdr(seladdr), .selacc(selacc), .selswap(selswap), .doswap(doswap), .selip1(selip1), .selip2(selip2), 
-		.curinsn(curinsn), .aluinsn(aluinsn));
+		.curinsn(curinsn), .aluinsn(aluinsn), .runio(runio));
+	
+	iocontroller sextium_iocontroller(.clock(clock), .reset(reset), .runio(runio), .acc(acc_out), .ioack(ioack),
+		.iobusy(iobusy), .io_read(io_read), .io_write(io_write), .acc_write(io_acc_write));
 	
 	mux16to4 insn_mux(.in(ir_out), .sel(curinsn), .out(insn));
 	
